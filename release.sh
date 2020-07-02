69e03916510ca5e6f14fcc860bf529fc2ab3dd3e
Arnab <arnab.compsc@gmail.com>
2020-07-02 14:17:11 +0530

    Added release.sh

---
+++ release.sh
@@ -0,0 +1,3 @@
+curl -T ~/.m2/repository/com/rudderstack/sdk/java/sdk/1.0.0/sdk-1.0.0.pom \
+  -u $BINTRAY_USER:$BINTRAY_PASSWORD \
+  https://api.bintray.com/content/rudderstack/rudderstack/sdk/1.0.0/sdk-1.0.0.pom
