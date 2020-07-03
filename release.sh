4750219566682719be7680ed94ba98ad570f905f
Arnab <arnab.compsc@gmail.com>
2020-07-03 09:12:36 +0530

    Interim update

--- release.sh
+++ release.sh
@@ -24,6 +24,6 @@ VERSION_NAME=1.0.0
 #   -u $BINTRAY_USER:$BINTRAY_PASSWORD \
 #   https://api.bintray.com/content/rudderstack/rudderstack/$PACKAGE_NAME/$VERSION_NAME/com/rudderstack/sdk/java/sdk-core/$VERSION_NAME/sdk-core-$VERSION_NAME.jar
 
-curl -T ~/.m2/repository/com/rudderstack/sdk/java/sdk/1.0.0/sdk-1.0.0.pom \
+curl -T ~/.m2/repository/com/rudderstack/sdk/java/rudderanalytics/1.0.0/rudderanalytics-1.0.0.pom \
   -u $BINTRAY_USER:$BINTRAY_PASSWORD \
-  https://api.bintray.com/maven/rudderstack/rudderstack/sdk/;publish=1/com/rudderstack/sdk/java/sdk/1.0.0/sdk-1.0.0.pom
+  https://api.bintray.com/maven/rudderstack/rudderstack/sdk/;publish=1/com/rudderstack/sdk/java/rudderanalytics/1.0.0/rudderanalytics-1.0.0.pom
