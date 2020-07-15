e950c1cf3787f99e9fc9d3584eb5b10fa90811ce
SAJAL MOHANTA <sajalmohanta@gmail.com>
2020-07-16 03:23:40 +0530

    Updated TestClass

--- rudderanalytics-client/src/main/java/com/rudderstack/sdk/java/TestClass.java
+++ rudderanalytics-client/src/main/java/com/rudderstack/sdk/java/TestClass.java
@@ -7,36 +7,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestClass {
-//    public static void main(String[] args) {
-//        RudderAnalytics analytics = RudderAnalytics.builder(
-//                "your_write_key",
-//                "https://data-plane-url"
-//        )
+//     public static void main(String[] args) {
+//         RudderAnalytics analytics = RudderAnalytics.builder(
+//                 "YOUR_WRITE_KEY",
+//                 "DATA_PLANE_URL"
+//         )
 //                .log(new Log() {
 //                    @Override
 //                    public void print(Level level, String s, Object... objects) {
 //                        System.out.println(s);
 //                    }
-//
+
 //                    @Override
 //                    public void print(Level level, Throwable throwable, String s, Object... objects) {
 //                        System.out.println(s);
 //                    }
 //                })
-//                .build();
-//
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("name", "Michael Bolton");
-//        map.put("email", "mbolton@example.com");
-//        analytics.enqueue(IdentifyMessage.builder()
-//                .userId("f4ca124298")
-//                .traits(map)
-//        );
-//
-//        analytics.enqueue(AliasMessage.builder("previousId")
-//                .userId("f4ca124298")
-//        );
-//
-//        System.out.println("Hello World!");
-//    }
+//                 .synchronize(true)
+//                 .build();
+
+//         Map<String, Object> map = new HashMap<>();
+//         map.put("name", "Michael Bolton");
+//         map.put("email", "mbolton@example.com");
+//         analytics.enqueue(IdentifyMessage.builder()
+//                 .userId("f4ca124298")
+//                 .traits(map)
+//         );
+
+//         analytics.enqueue(AliasMessage.builder("previousId")
+//                 .userId("f4ca124298")
+//         );
+//         System.out.println("Pre-Flush");
+//         analytics.flush();
+//         analytics.blockFlush();
+//         System.out.println("Hello World!");
+//     }
 }
