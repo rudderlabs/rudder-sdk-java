bce908f67820ae5e0d78cd1447b26e8b5ebbbd9b
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2022-12-19 13:16:45 +0530

    feat!: bundle the `gzip` support inside the core SDK (#42)
    * feat!: bundle the `gzip` support inside the core SDK
    
    Now, GZIP will be enabled by default and have also introduced an API to toggle the GZIP default value, called `enableGZIP`. If user continues to use the `client` API to pass the OkHttpClient then `enableGZIP` API value will not be taken into consideration.
    
    BREAKING CHANGE: Now user who are using the Java SDK, with self-hosted dataPlane and without their own custom `OkHttpClient` (passed using `client` API)` needs to make sure that their server supports GZIP i.e., their server version is above 1.4, else they could either use `enableGZIP` API or pass a custom OkHttpClient instance without the GZIP interceptor.
    
    * refactor: change API name from `enableGZIP` to `setGZIP`

--- analytics/src/main/java/com/rudderstack/sdk/java/analytics/RudderAnalytics.java
+++ analytics/src/main/java/com/rudderstack/sdk/java/analytics/RudderAnalytics.java
@@ -148,6 +148,7 @@ public class RudderAnalytics {
     private List<Callback> callbacks;
     private int queueCapacity;
     private boolean forceTlsV1 = false;
+    private boolean gzip = true;
 
     Builder(String writeKey) {
       if (writeKey == null || writeKey.trim().length() == 0) {
@@ -162,6 +163,12 @@ public class RudderAnalytics {
         throw new NullPointerException("Null client");
       }
       this.client = client;
+      return this;
+    }
+
+    /** Disable the GZIP client. */
+    public Builder setGZIP(boolean gzip) {
+      this.gzip = gzip;
       return this;
     }
 
@@ -358,7 +365,7 @@ public class RudderAnalytics {
       }
 
       if (client == null) {
-        client = Platform.get().defaultClient();
+        client = Platform.get().defaultClient(this.gzip);
       }
 
       if (log == null) {
